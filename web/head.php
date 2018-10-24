10714bf18536121ef865585cd25cac132549527a
Fabien Leite <fabienleite@protonmail.com>
2018-10-24 15:18:49 +0200

    Finish creation setup

--- web/head.php
+++ web/head.php
@@ -7,5 +7,21 @@
     <meta name="author" content="Fabien Leite, Rémi Millerand">
     <meta name="description" content="Jean Mi Construction challenges by Fabien Leite & Rémi Millerand">
     <title>Jean Mi Construction Rebirth</title>
+
+    <link rel=" icon" type="image/png" href="/img/building_cloud.png"/>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="css/fonts.css" rel="stylesheet" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
 </head>
-<body>
+
+<body id="page-top">
